3f9ec11b6c8784d4b600f02ced53b7fb2a4c97fa
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-08-30 18:09:41 +0545

    working utils.py

--- backend/main/utils/utils.py
+++ backend/main/utils/utils.py
@@ -1,15 +1,19 @@
 import pdfplumber
+import os
 
 def extract_text_from_pdf(file_path):
-    text =''
+    output_dir = r"F:\Personal Project\study-buddy-mvp\backend\notes\txt"
+    base_name = os.path.splitext(os.path.basename(file_path))[0]
+    output_path = os.path.join(output_dir, base_name + ".txt")
+
+    text = ''
     with pdfplumber.open(file_path) as pdf:
         for i, page in enumerate(pdf.pages):
             content = page.extract_text()
             if content:
-                text+=content + '\n'
-            
-    return text
+                text += content + '\n'
+    
+    with open(output_path, 'w', encoding='utf-8') as file:
+        file.write(text)
 
-filename = r"F:\Personal Project\study-buddy-mvp\backend\notes\txt\resume.txt"
-with open(filename, 'w', encoding='utf-8') as file:
-    file.write(extract_text_from_pdf("F:\Resume - Bibek Adhikari.pdf"))
+extract_text_from_pdf(r"F:\Resume - Bibek Adhikari.pdf")
